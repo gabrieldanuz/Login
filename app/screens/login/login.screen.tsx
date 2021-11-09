f395824c80ee62097a8ed33f87169918d89ae0b8
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2021-11-08 23:16:35 -0300

    App

--- app/screens/login/login.screen.tsx
+++ app/screens/login/login.screen.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
-import { Card, TextInput } from "react-native-paper";
+import { SafeAreaView, View } from "react-native";
+import { Card, TextInput, Button } from "react-native-paper";
+import { loginStyle } from "./login.style";
 
 export const LoginScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={loginStyle.content}>
+      <View>
       <Card>
         <Card.Title title= "Delivery App"></Card.Title>
         <Card.Content>
           <TextInput label="Email" keyboardType="email-address"></TextInput>
-          <TextInput></TextInput>
+          <TextInput label="Password" secureTextEntry={true}></TextInput>
+          <Button uppercase={false}>Forgot Email/Password</Button>
+          <Button mode="contained">Login</Button>
+          <Button>Register</Button>
         </Card.Content>
       </Card>
+      </View>
     </SafeAreaView>
   )
 }
